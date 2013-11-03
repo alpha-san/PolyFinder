e13efaf5a10b66c93cc258522f046f1ecaee3328
Allan Farinas <awesomeplusplus@riseup.net>
2013-11-02 18:54:47 -0700

    Able to show events from firebase

--- assets/leaflet/map.js
+++ assets/leaflet/map.js
@@ -8,10 +8,35 @@ var map = L.map('map').setView([5, 5], 2);
             tms: true
         }).addTo(map);
 	map.panTo(new L.LatLng(-65.737, -73.923));
+	
+// Firebase stuff
+var polyfinderData = new Firebase('https://polyfindertest.firebaseio.com/events');
+polyfinderData.on('value', function(snapshot) {
+	console.log(snapshot.val());
+	var events = snapshot.val();
+	for (var i = 0; i < events.length; i++) {
+		createEvent(events[i].message, new L.LatLng(events[i].coordinates.x, events[i].coordinates.y), events[i].id);
+	}
+});
+
+map.on('click', onMapClick);  
 
 function createEvent(txt, lat, id){
 	var marker = L.marker(lat).addTo(map);
 	marker.bindPopup(txt);
-        marker.eventID = id;
+    marker.eventID = id;
+    console.log(marker);
 }
 
+// Get coordinates
+//-1 - top, -179 - left, 31 - right, -89 - bottom  
+// b8: (-44.80912, -121.37695), b94: (-41.40978, -106.30371)
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+function addButtons() {
+	var btn=document.createElement("BUTTON");
+	var t=document.createTextNode("CLICK ME");
+	btn.appendChild(t);
+}
--- src/com/csupomona/polyfinder/LogPoint.java
+++ src/com/csupomona/polyfinder/LogPoint.java
@@ -1,6 +1,5 @@
 package com.csupomona.polyfinder;
 
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
